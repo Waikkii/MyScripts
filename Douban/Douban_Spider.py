29ddbbc0c599b7edaae509899e730a1e2b5a99f6
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-08-05 13:45:58 +0800

    添加高匿IP代理池

--- Douban/Douban_Spider.py
+++ Douban/Douban_Spider.py
@@ -2,53 +2,51 @@
 import random
 import requests
 import re
-# from fake_useragent import UserAgent
 import time
 import os
+import string
 
 # Env环境设置 通知服务
 # export DOUBAN_cookie='xxxxxxxxxxxxxx'                            # （少于8页不需要）豆瓣cookie，自行登录抓包;
 # export DOUBAN_Keyword='xx@xx@x@xxx'                              # 检索关键词，使用@连接;
 # export DOUBAN_page='x'                                           # 豆瓣小组检索页数（过多可能被封ip）;
 # export DOUBAN_barkkey='xxxxxxxxxxxxx$xxxxxxxxxxxxxx'             # bark服务,苹果商店自行搜，支持多个用户推送;
-# 建议cron */15 * * * *
+# export DOUBAN_IP='ip:port/get'                                   # ip代理池（如果不需要可以自行修改line 98/99）;
+# 建议cron */30 * * * *
 
-def get_high_stash_IPs():
-    ip_num = 30
-    ip_url = 'http://www.xiladaili.com/api/?uuid=92968207a8d44443b4305cae050901c4&num='+str(ip_num)+'&place=中国&category=1&protocol=2&sortby=0&repeat=1&format=3&position=1' 
-    print('IP url is: ', ip_url)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
-    }
-    get_json = requests.get(ip_url, headers=headers).text
-    return get_json.split(' ')
+def get_ip_json(ip):
+    return {'http':'http://' + ip}#{'http':'http://' + ip, 'https':'https://' + ip}
 
-def get_random_proxies(ip_list):
-    proxy_list = []
-    random.shuffle(ip_list)
-    for i in range(len(ip_list)):
-        proxy_one = {'http':'http://' + ip_list[i], 'https':'https://' + ip_list[i]}
-        proxy_list.append(proxy_one)
-    return proxy_list
-
-def check_ip(proxy_list):
-    proxy_checked_list = []
+def check_ip(proxy_ip):
     check_url = 'http://icanhazip.com/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    for item in proxy_list:
-        try:
-            requests.adapters.DEFAULT_RETRIES = 3
-            res = requests.get(check_url, headers=headers, proxies=item, timeout=3)
-            proxyIP = res.text[0:-1]
-            print(proxyIP, item['https'].split('//')[1].split(':')[0])
-            if (proxyIP == item['https'].split('//')[1].split(':')[0]):
-                print("Proxy IP:'" + proxyIP + " success!")
-                proxy_checked_list.append(item)
-            else:
-                print("Check: Proxy IP failure!")
-        except Exception as e:
-            print("ERROR: Proxy IP failure!", e)
-    return proxy_checked_list
+    try:
+        requests.adapters.DEFAULT_RETRIES = 3
+        res = requests.get(check_url, headers=headers, proxies=proxy_ip, timeout=3)
+        proxyIP = res.text[0:-1]
+        # print(proxyIP, proxy_ip['https'].split('//')[1].split(':')[0])
+        if (proxyIP == proxy_ip['http'].split('//')[1].split(':')[0]):
+            print("Proxy IP:" + proxyIP + " success!")
+            return True
+        else:
+            print("Check: Proxy IP failure!")
+            return False
+    except Exception as e:
+        print("ERROR: Proxy IP failure!", e)
+        return False
+
+# 高匿代理一定需要
+def get_high_stash_IP():
+    ip_url = DOUBAN_IP_GET
+    ip = ""
+    while True:
+        get_json = requests.get(ip_url).json()
+        ip =  get_ip_json(get_json["proxy"])
+        # print("In While:", ip)
+        if check_ip(ip) == True:
+            break
+        time.sleep(3)
+    return ip
 
 def get_random_ua():
     first_num = random.randint(55, 62)
@@ -65,50 +63,62 @@ def get_random_ua():
                   )
     return ua
 
-def get_douban_works(num, groupnumber, ck):
-    num_norm = (num-1)*25
-    douban_url = 'https://www.douban.com/group/'+groupnumber+'/discussion?start='+str(num_norm)
-    # try:
-    #     Random_UA = UserAgent().random
-    # except:
-    #     Random_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
+def get_random_ck():
+    twond = str(random.randint(2, 4))
+    last = ''.join(str(random.choice(range(7))) for _ in range(7))
+    final = ''.join(random.sample(string.digits+string.ascii_letters, 11))
+    dbcl2 = '2'+twond+last+':'+final
+    bid = ''.join(random.sample(string.digits+string.ascii_letters, 11))
+    ck = ''.join(random.sample(string.digits+string.ascii_letters, 4))
+    return 'bid=' + bid + '; dbcl2="' + dbcl2 + '"; ck=' + ck
 
-    # Random_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'
+def get_douban_works(num, groupnumber):
+    num_norm = num*25
+    douban_url = 'https://www.douban.com/group/'+groupnumber+'/discussion?start='+str(num_norm)
     Random_UA = get_random_ua()
+    Random_CK = get_random_ck()
     print('Page Group:', groupnumber)
     print("生成随机UA：", Random_UA)
 
-    if num<=8:
-        headers = {
-            'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,ja;q=0.7',
-            'Connection': 'keep-alive',
-            'User-Agent': Random_UA
-        }
+    if "DOUBAN_cookie" in os.environ:
+        DOUBAN_cookie = os.environ["DOUBAN_cookie"]
+        # print("已获取并使用Env环境，DOUBAN_cookie=", DOUBAN_cookie)
+        print("使用指定CK：", DOUBAN_cookie)
     else:
-        headers = {
-            'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,ja;q=0.7',
-            'Connection': 'keep-alive',
-            'User-Agent': Random_UA,
-            'Cookie' : ck
-        }
-    
-    douban_res = requests.get(douban_url, headers=headers)
-    pattern1 = re.compile(r'<td class="title">(.*?)</td>', re.S | re.M)
-    rowinfo = pattern1.findall(douban_res.text)
-    work_list = []
-    for item in rowinfo:
-        if "【作业】" in item:
-            work_list.append(item)
-    return work_list
+        print("使用随机CK：", Random_CK)
 
-def get_final_works(page_num, ck):
+    headers = {
+        'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,ja;q=0.7',
+        'Connection': 'keep-alive',
+        'Cookie' : Random_CK,
+        'Host': 'www.douban.com',
+        'Referer': 'https://accounts.douban.com/',
+        'User-Agent': Random_UA,
+    }
+
+    proxy_IP = get_high_stash_IP()
+    douban_res = requests.get(douban_url, headers=headers, proxies=proxy_IP)
+    # print(douban_res.text)
+    if '检测到有异常请求从你的 IP 发出' in douban_res.text:
+        bark_post('IP已被封', '请更换ck或者使用代理ip', DOUBAN_barkkey)
+        return []
+    else:
+        pattern1 = re.compile(r'<td class="title">(.*?)</td>', re.S | re.M)
+        rowinfo = pattern1.findall(douban_res.text)
+        work_list = []
+        for item in rowinfo:
+            if "【作业】" in item:
+                work_list.append(item)
+        return work_list
+
+def get_final_works(page_num):
     work_total_list = []
     for i in range(page_num):
         print("Get Page:", i+1)
-        work_total_list.extend(get_douban_works(i,'698716', ck))
-        time.sleep(3)
-        work_total_list.extend(get_douban_works(i,'what2buy', ck))
-        time.sleep(3)
+        work_total_list.extend(get_douban_works(i,'698716'))
+        time.sleep(5)
+        work_total_list.extend(get_douban_works(i,'what2buy'))
+        time.sleep(5)
     return {}.fromkeys(work_total_list).keys()
 
 def choose_work(list, key_list):
@@ -118,7 +128,7 @@ def choose_work(list, key_list):
     work_to_send = []
     for item in list:
         for key in key_list:
-            if key in item.lower():
+            if key in item:
                 topic = pattern_title.findall(item)[0]
                 if topic in data:
                     print("作业已经存在不推送：", topic)
@@ -128,11 +138,9 @@ def choose_work(list, key_list):
     return work_to_send
 
 def bark_post(Subject, Message, Sckey):
-    Sckey_list = Sckey.split('&')
-    for i in range(len(Sckey_list)):
-        url = 'https://api.day.app/' + Sckey_list[i] + '/' + Subject + '?url=' + Message
-        print(url)
-        requests.get(url)
+    url = 'https://api.day.app/' + Sckey + '/' + Subject + '?url=' + Message
+    print(url)
+    r = requests.get(url)
 
 def send_nofity(list, sendkey):
     if list==[]:
@@ -146,23 +154,17 @@ def send_nofity(list, sendkey):
         hrefurl = pattern_href.findall(item)[0]
         print(topic)
         print(hrefurl)
-        bark_post(topic.replace('/', ''), hrefurl, sendkey)
+        bark_post(topic, hrefurl, sendkey)
         print("推送成功！")
         with open('douban_work.txt', 'r+',newline='',encoding='utf-8') as f:
+            content = f.read()
             f.seek(0, 2)
             f.write(topic + '\n')
         print("写入数据成功！")
         time.sleep(5)
     return
-
  
 if __name__ == '__main__':
-    # 高匿代理暂时不需要
-    # IP_list = get_high_stash_IPs()
-    # RANDOM_IP_LIST = get_random_proxies(IP_list)
-    # CHECKED_IP_LIST = check_ip(RANDOM_IP_LIST)
-
-
     if "DOUBAN_Keyword" in os.environ:
         DOUBAN_Keyword = os.environ["DOUBAN_Keyword"]
         DOUBAN_Keyword = DOUBAN_Keyword.split('@')
@@ -173,9 +175,9 @@ if __name__ == '__main__':
     if "DOUBAN_barkkey" in os.environ:
         DOUBAN_barkkey = os.environ["DOUBAN_barkkey"]
         print("已获取并使用Env环境，DOUBAN_barkkey=", DOUBAN_barkkey)
-    if "DOUBAN_cookie" in os.environ:
-        DOUBAN_cookie = os.environ["DOUBAN_cookie"]
-        print("已获取并使用Env环境，DOUBAN_cookie=", DOUBAN_cookie)
+    if "DOUBAN_IP_GET" in os.environ:
+        DOUBAN_IP_GET = os.environ["DOUBAN_IP_GET"]
+        print("已获取并使用Env环境，DOUBAN_IP_GET=", DOUBAN_IP_GET)
 
     filename = 'douban_work.txt'
     if os.path.exists(os.getcwd()+'/'+filename)==False:
@@ -184,6 +186,6 @@ if __name__ == '__main__':
         file.close()
 
 
-    final_work = get_final_works(DOUBAN_page, DOUBAN_cookie)
+    final_work = get_final_works(DOUBAN_page)
     final_send = choose_work(final_work, DOUBAN_Keyword)
     send_nofity(final_send, DOUBAN_barkkey)
